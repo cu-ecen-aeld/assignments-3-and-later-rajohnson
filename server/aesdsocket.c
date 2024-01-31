ac08d37c63821ec2ac75621ef2bd1b90e5df703b
Rob Johnson <johnson.rob.alan@gmail.com>
2024-01-31 05:23:40 +0000

    move connection handling into a function so that it can become a seperate process.

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -41,6 +41,65 @@ void signal_handler(int signal) {
 	exit(EXIT_SUCCESS);
 }
 
+void connection_handler(int client_fd, struct sockaddr_storage their_addr) {
+	// Log message to the syslog “Accepted connection from xxx” where XXXX is the IP address of the connected client.
+	char client_ip[INET6_ADDRSTRLEN];
+
+	if(inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr*)&their_addr), client_ip, sizeof client_ip) == NULL) {	
+		syslog(LOG_ERR, "inet_ntop failed");
+		exit(-1);
+	}
+
+	syslog(LOG_USER, "Accepted connection from %s", client_ip); 
+
+	// Receive data over the connection and appends to file /var/tmp/aesdsocketdata, creating this file if it doesn’t exist. 
+	//Your implementation should use a newline to separate data packets received.  In other words a packet is considered complete when a newline character is found in the input receive stream, and each newline should result in an append to the /var/tmp/aesdsocketdata file.
+	// You may assume the data stream does not include null characters (therefore can be processed using string handling functions).
+	// You may assume the length of the packet will be shorter than the available heap size.  In other words, as long as you handle malloc() associated failures with error messages you may discard associated over-length packets.
+	int rxdata_fd = open("/var/tmp/aesdsocketdata", O_CREAT | O_RDWR | O_APPEND, S_IRWXU | S_IRWXG | S_IRWXO);	
+	if(rxdata_fd < 0) {
+		syslog(LOG_ERR, "error opening log file /var/tmp/aesdsocketdata");
+		exit(-1);
+	}
+	
+	const int BUF_LEN = 100;
+	char rx_data[BUF_LEN];
+	memset(rx_data, 0, BUF_LEN);
+	int numbytes;
+	while((numbytes = recv(client_fd, rx_data, BUF_LEN - 1, 0)) > 0) {
+		syslog(LOG_USER, "recieved[%li, %i]: %s", strlen(rx_data), numbytes, rx_data);
+		if(write(rxdata_fd, rx_data, strlen(rx_data)) != (ssize_t)strlen(rx_data)) {
+			syslog(LOG_ERR, "error writing data to file.");
+			exit(-1);
+		}
+
+		if(rx_data[numbytes - 1] == '\n') {
+			break;
+		}
+
+		memset(rx_data, 0, BUF_LEN);
+	}
+
+	// Return the full content of /var/tmp/aesdsocketdata to the client as soon as the received data packet completes.
+	// move back to the beginning of the file
+	lseek(rxdata_fd, 0, SEEK_SET);
+
+	char tx_data[BUF_LEN];
+	while((numbytes = read(rxdata_fd, tx_data, BUF_LEN)) > 0) {
+		send(client_fd, tx_data, numbytes, 0);
+	}	
+
+	fdatasync(rxdata_fd);	
+	close(rxdata_fd);
+
+	// Log message to the syslog “Closed connection from XXX” where XXX is the IP address of the connected client.
+	close(client_fd);
+	syslog(LOG_USER, "Closed connection from %s", client_ip);
+
+	exit(0);
+}
+
+
 int main(int argc, char **argv) {
 	bool is_daemon = false;
 	openlog("aesdsocket", 0, LOG_USER);
@@ -128,66 +187,14 @@ int main(int argc, char **argv) {
 
 	struct sockaddr_storage their_addr;
 	socklen_t addr_size = sizeof their_addr;
+
 	int new_socket = accept(server_fd, (struct sockaddr*)&their_addr, &addr_size);
 	if(new_socket < 0) {
 		syslog(LOG_ERR, "accept failed");
 		return -1;
 	}
 
-	// Log message to the syslog “Accepted connection from xxx” where XXXX is the IP address of the connected client.
-	char client_ip[INET6_ADDRSTRLEN];
-	
-	if(inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr*)&their_addr), client_ip, sizeof client_ip) == NULL) {	
-		syslog(LOG_ERR, "inet_ntop failed");
-		return -1;
-	}
-
-	syslog(LOG_USER, "Accepted connection from %s", client_ip); 
-
-	// Receive data over the connection and appends to file /var/tmp/aesdsocketdata, creating this file if it doesn’t exist. 
-	//Your implementation should use a newline to separate data packets received.  In other words a packet is considered complete when a newline character is found in the input receive stream, and each newline should result in an append to the /var/tmp/aesdsocketdata file.
-	// You may assume the data stream does not include null characters (therefore can be processed using string handling functions).
-	// You may assume the length of the packet will be shorter than the available heap size.  In other words, as long as you handle malloc() associated failures with error messages you may discard associated over-length packets.
-	int rxdata_fd = open("/var/tmp/aesdsocketdata", O_CREAT | O_RDWR | O_APPEND, S_IRWXU | S_IRWXG | S_IRWXO);	
-	if(rxdata_fd < 0) {
-		syslog(LOG_ERR, "error opening log file /var/tmp/aesdsocketdata");
-		return -1;
-	}
-	
-	const int BUF_LEN = 100;
-	char rx_data[BUF_LEN];
-	memset(rx_data, 0, BUF_LEN);
-	int numbytes;
-	while((numbytes = recv(new_socket, rx_data, BUF_LEN - 1, 0)) > 0) {
-		syslog(LOG_USER, "recieved[%li, %i]: %s", strlen(rx_data), numbytes, rx_data);
-		if(write(rxdata_fd, rx_data, strlen(rx_data)) != (ssize_t)strlen(rx_data)) {
-			syslog(LOG_ERR, "error writing data to file.");
-			return -1;
-		}
-
-		if(rx_data[numbytes - 1] == '\n') {
-			break;
-		}
-
-		memset(rx_data, 0, BUF_LEN);
-	}
-
-	// Return the full content of /var/tmp/aesdsocketdata to the client as soon as the received data packet completes.
-	// move back to the beginning of the file
-	lseek(rxdata_fd, 0, SEEK_SET);
-
-	char tx_data[BUF_LEN];
-	while((numbytes = read(rxdata_fd, tx_data, BUF_LEN)) > 0) {
-		send(new_socket, tx_data, numbytes, 0);
-	}	
-
-	fdatasync(rxdata_fd);	
-	close(rxdata_fd);
-
-	// Log message to the syslog “Closed connection from XXX” where XXX is the IP address of the connected client.
-	close(new_socket);
-	syslog(LOG_USER, "Closed connection from %s", client_ip);
-
+	connection_handler(new_socket, their_addr);
 	// todo Restarts accepting connections from new clients forever in a loop until SIGINT or SIGTERM is received (see below).
 
 	return 0;
