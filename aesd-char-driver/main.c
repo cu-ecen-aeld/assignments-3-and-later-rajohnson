953d62fd26052db931fcfa11ac2438b9914f2d28
Rob Johnson <johnson.rob.alan@gmail.com>
2024-04-08 01:08:05 +0000

    resolve problem with const not propogating.

--- aesd-char-driver/main.c
+++ aesd-char-driver/main.c
@@ -134,7 +134,7 @@ ssize_t aesd_write(struct file *filp, const char __user *buf, size_t count,
 		// data is terminated with newline - push to buffer
 		//PDEBUG("Writing %zu bytes to buffer: %s", total_count, aesd_device.temp_write_data);
 		struct aesd_buffer_entry entry = {.size=total_count, .buffptr=aesd_device.temp_write_data};
-		char* old_data = aesd_circular_buffer_add_entry(&aesd_device.buffer, &entry);
+		const char* old_data = aesd_circular_buffer_add_entry(&aesd_device.buffer, &entry);
 		kfree(old_data); // can be passed to kfree, even if NULL
 		aesd_device.temp_write_data = NULL; // Has been saved to buffer.
 	}
